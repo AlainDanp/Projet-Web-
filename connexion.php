4da89e99dbd322173a33eaa7a8e34d84471c9c38
DANP <alain.datouo@2027.ucac-icam.com>
2024-03-06 16:29:49 +0100

    Initial commit

---
+++ connexion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link href="style_formul.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="drop"></div>
+    <div class="conent">
+        <h2> Connexion </h2>
+        <hr />
+        <form>
+            <div class="box">
+                <input type="text" placeholder="Nom d'utilisateur" required>
+            </div>
+            <div class="box">
+                <input type="password" placeholder="Mot de passe" required>
+            </div>
+            <div>
+                <a href="inscription.php" class="lien2"> je n'ai de compte</a>
+            </div>
+            <button type="submit" class="btn"> Inscription</button>
+        </form>
+    </div>
+    <div class="text">
+        <h2> Bienvenue</h2>
+        <p>page de connexion </p>
+    </div>
+</div>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link href="style_formul.css" rel="stylesheet">
+</head>
+<body class="bodo">
+    <div class="container">
+        <div class="drop"></div>
+        <div class="conent">
+            <h2> Inscription </h2>
+            <hr />
+            <form>
+                <div class="box">
+                    <input type="text" placeholder="Nom d'utilisateur. EX: JOHSON" required>
+                </div>
+                <div class="box">
+                    <input type="email" placeholder="Prénom d'util. EX: Alain" required>
+                </div>
+                <div class="box">
+                    <input type="text" placeholder="Localisation. Ex: Yaoundé" required>
+                </div>
+                <div class="box">
+                    <input type="password" placeholder="Mot de passe" required>
+                </div>
+                <div>
+                <a href="connexion.php" class="lien"> j'ai un compte</a>
+                </div>
+                <button type="submit" class="btn"> Inscription</button>
+            </form>
+        </div>
+        <div class="text">
+            <img src="assest/">
+            <h2> Bienvenue</h2>
+            <p> sur notre page d'inscripition </p>
+        </div>
+    </div>
+</body>
+</html>
